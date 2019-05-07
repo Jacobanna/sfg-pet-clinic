2a6c78ca46b62b0cce3323e52d1ec0b3bee873b6
Jacobanna <pieprzyk.jakub@gmail.com>
2019-05-07 17:34:15 +0200

    Updated owner with JPA entity mappings. Closes #36.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,7 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
+
+    //Hibernate actually does what we did here (changing camelCase to snake_case in db)
+    //Redundant here
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
